89ada4375f639840c80d60278a3268c3be5a1556
OLLO77 <petkooleg@gmail.com>
2023-03-13 16:00:51 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useEffect, useRef, useState} from "react";
+import * as tt from '@tomtom-international/web-sdk-maps'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const mapElement = useRef()
+    const [map, setMap] = useState({})
+    const [longitude, setLongitude] = useState(-2.983333)
+    const [latitude, setLatitude] = useState(53.400002)
+
+    useEffect(() => {
+        let map = tt.map({
+            key: process.env.REACT_APP_TON_TON_API_KEY,
+            container: mapElement.current,
+            stylesVisibility: {
+                trafficIncidents: true,
+                trafficFlow: true
+            },
+            center: [longitude, latitude],
+            zoom: 10
+        });
+        setMap(map)
+        return () => map.remove()
+    }, [longitude, latitude])
+    return (
+        <div className="app">
+            <div ref={mapElement} className='map'>
+                <div className="search=-bar">
+                    <h1>Where to?</h1>
+                    <input id="longitude" className="longitude" placeholder="Put in Longitude"
+                           onChange={event => setLongitude(event.target.value)}/>
+                    <input id="latitude" className="latitude" placeholder="Put in Latitude"
+                           onChange={event => setLatitude(event.target.value)}/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default App
